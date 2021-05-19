3cf1585958b7cfa92458ebf3f89d0ee074b73464
mission-deny-the-mission <62898307+mission-deny-the-mission@users.noreply.github.com>
2021-05-19 22:06:20 +0100

    Add check to make sure that the username is not already taken.

--- sign_up_handle.php
+++ sign_up_handle.php
@@ -13,6 +13,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $password_hash = password_hash($password, PASSWORD_BCRYPT);
 
+    $previous_usernames = $conn->query("SELECT username FROM user;");
+
+    if ($previous_usernames->num_rows > 0)
+    {
+        while ($row = $previous_usernames->fetch_assoc())
+        {
+            if ($row["username"] === $username)
+            {
+                echo "That username is taken.";
+                $conn->close();
+                exit();
+            }
+        }
+    }
+
     // TODO: find out if this is the best way to check for null
     if ($email == "")
     {
@@ -26,11 +41,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($conn->query($query) === TRUE) {
         echo "Sign up was successful<br>";
         header("Location: sign_up_complete.html");
+        $conn->close();
         exit();
     } else {
         echo $query;
-        echo "<br>";
-        echo "an error occured executing the query<br>";
+        echo "<br><br>";
+        echo "an error occured executing the query<br><br>";
         echo mysqli_error($conn);
     }
 
@@ -39,21 +55,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 } else {
     echo "You should not be here";
 }
-
-/*
-$query = "INSERT INTO users (user_name, ";
-
-if(!htmlspecialchars($_POST["email"]) == "")
-{
-    $query += "email, ";
-    
-}
-$query += "password_hash) VALUES (";
-
-$query += htmlspecialchars($_POST["user_name"]) + ", ";
-if(!htmlspecialchars($_POST["email"]) == "")
-{
-    $query += htmlspecialchars($_POST)
-}
-*/
 ?>
