ecbfa24ef41c64da15f8a0d78080cc1680ff07d1
mission-deny-the-mission <62898307+mission-deny-the-mission@users.noreply.github.com>
2021-06-01 00:04:04 +0100

    Add button to go back to home page on log out page

--- log out.php
+++ log out.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+        <link rel="stylesheet" href="main.css">
         <title>Log out page</title>
     </head>
     <body>
@@ -7,5 +8,6 @@
         session_destroy();
         echo "You have successfully logged out"
         ?>
+        <button onclick="document.location='index.html'">Back to home page</button>
     </body>
 <html>
