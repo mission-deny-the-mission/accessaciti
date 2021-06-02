f6d6dc416e19075274b70798d21a3215c1e3aaa2
mission-deny-the-mission <62898307+mission-deny-the-mission@users.noreply.github.com>
2021-06-02 17:39:41 +0100

    add field ids to change password.php

--- change password.php
+++ change password.php
@@ -16,11 +16,11 @@ if (!$_SESSION["valid"]) {
         <h1>Change password:</h1>
         <form>
             <label for="old password">Old password</label><br>
-            <input type="text" name="old password"><br>
+            <input type="text" name="old password" id="old password"><br>
             <label for="new password">New password</label><br>
-            <input type="password" name="new password"><br>
+            <input type="password" name="new password" id="new password"><br>
             <label for="repeat password">Verify password:</label><br>
-            <input type="password" name="verify password"><br>
+            <input type="password" name="verify password" id="verify password"><br>
             <input type="submit" name="Change password">
         </form>
     </body>
