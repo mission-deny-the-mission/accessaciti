7b0cbfb008ca8da07386528d5795518a1ced62db
mission-deny-the-mission <mission.deny.the.mission@gmail.com>
2021-06-02 19:20:23 +0100

    Add favorites column to issue.php but only for logged in users

--- issues.php
+++ issues.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 require("connect to database.php");
 $conn = connect_to_database();
 
@@ -9,42 +11,66 @@ WHERE issue.location_id = location.location_id;";
 
 $result = $conn->query($queryString);
 
-if(!$result) {
+if (!$result) {
     echo "an error occured running the query";
     exit();
+}
+
+if (isset($_SESSION["valid"]) && $_SESSION["valid"]) {
+    $query2 = "SELECT issue_id FROM FavoriteIssues WHERE user_id = " . $_SESSION["userid"] . ";";
+    $favorites = $conn->query($query2);
+    $hasfavorites = true;
+} else {
+    $hasfavorites = false;
 }
 ?>
 
 <html>
-    <head>
-        <link rel="stylesheet" href="main.css">
-        <title>Saved issues</title>
-    </head>
-    <body>
-        <h1>Saved issues</h1>
-        <table>
-            <tr>
-                <th>Issue ID</th>
-                <th>Description</th>
-                <th>illegality</th>
-                <th>longitude</th>
-                <th>latitude</th>
-                <th>current rating</th>
-            </tr>
+
+<head>
+    <link rel="stylesheet" href="main.css">
+    <title>Saved issues</title>
+</head>
+
+<body>
+    <h1>Saved issues</h1>
+    <table>
+        <tr>
+            <th>Issue ID</th>
+            <th>Description</th>
+            <th>illegality</th>
+            <th>longitude</th>
+            <th>latitude</th>
+            <th>current rating</th>
             <?php
-            if($result->num_rows > 1) {
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr>";
-                    echo "<td>" . $row["issue_id"] . "</td>";
-                    echo "<td>" . $row["issue_description"] . "</td>";
-                    echo "<td>" . $row["illegality"] . "</td>";
-                    echo "<td>" . $row["longitude"] . "</td>";
-                    echo "<td>" . $row["latitude"] . "</td>";
-                    echo "<td>" . $row["current_rating"] . "</td>";
-                    echo "</tr>";
-                }
+            if ($hasfavorites) {
+                echo "<th>favorite</th>";
             }
             ?>
-        </table>
-    </body>
+        </tr>
+        <?php
+        if ($result->num_rows > 1) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["issue_id"] . "</td>";
+                echo "<td>" . $row["issue_description"] . "</td>";
+                echo "<td>" . $row["illegality"] . "</td>";
+                echo "<td>" . $row["longitude"] . "</td>";
+                echo "<td>" . $row["latitude"] . "</td>";
+                echo "<td>" . $row["current_rating"] . "</td>";
+                if ($hasfavorites) {
+                    $favorite_id = $favorites->fetch_assoc()["issue_id"];
+                    if ($favorite_id == $row["issue_id"]) {
+                        echo "<td>Yes</td>";
+                    } else {
+                        echo "<td>No</td>";
+                    }
+                }
+                echo "</tr>";
+            }
+        }
+        ?>
+    </table>
+</body>
+
 </html>
--- logged_in.php
+++ logged_in.php
@@ -1,19 +1,9 @@
 <?php
 session_start();
-try
-{
-    if ($_SESSION["valid"])
-    {
-        echo "You have logged in successfully<br>";
-        echo "Your username is " . $_SESSION["username"];
-    }
-    else
-    {
-        echo "You are not logged in";
-    }
+
+if (isset($_SESSION["valid"]) && $_SESSION["valid"]) {
+    echo "You have logged in successfully<br>";
+    echo "Your username is " . $_SESSION["username"];
+} else {
+    echo "You are not logged in";
 }
-catch (Exception $e)
-{
-    echo "You are deffo not logged in";
-}
-?>
