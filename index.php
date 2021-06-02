ab3944cc7c3e6bf3a9f80e5b9f634ae94faefe64
mission-deny-the-mission <mission.deny.the.mission@gmail.com>
2021-06-02 19:31:26 +0100

    reformat code for index.php. Remove home button

--- index.php
+++ index.php
@@ -9,39 +9,41 @@ if (isset($_SESSION["valid"]) && $_SESSION["valid"]) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>AccessaCita</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">    
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
-    <div class="wrapper">
-        <div class="logo">
-            <img src="logo_small.png" alt="">
+        <div class="wrapper">
+            <div class="logo">
+                <img src="logo_small.png" alt="">
+            </div>
+            <ul class="nav-area">
+                <li><a href="#">About</a></li>
+                <?php
+                if (!$loggedIn) {
+                    echo "<li><a href='login.php'>Login</a></li>\n";
+                    echo "<li><a href='Singup.html'>Sign Up</a></li>\n";
+                } else {
+                    echo "<li><a href='change password.php'>Change Password</a></li>\n";
+                    echo "<li><a href='log out.php'>Logout</a></li>\n";
+                }
+                ?>
+                <li><a href="#">Contact</a></li>
+            </ul>
         </div>
-<ul class="nav-area">
-<li><a href="#">Home</a></li>
-<li><a href="#">About</a></li>
-<?php
-if (!$loggedIn) {
-    echo "<li><a href='login.php'>Login</a></li>\n";
-    echo "<li><a href='Singup.html'>Sign Up</a></li>\n";
-} else {
-    echo "<li><a href='change password.php'>Change Password</a></li>\n";
-    echo "<li><a href='log out.php'>Logout</a></li>\n";
-}
-?>
-<li><a href="#">Contact</a></li>
-</ul>
-</div>
-<div class="welcome-text">
-        <h1>
-Here To <span>Help</span></h1>
-<a href="#">Contact US</a>
-    </div>
-</header>
+        <div class="welcome-text">
+            <h1>
+                Here To <span>Help</span></h1>
+            <a href="#">Contact US</a>
+        </div>
+    </header>
 
 </body>
+
 </html>
