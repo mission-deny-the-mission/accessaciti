fe402ec880d21a55c8ecb4ca978f55b7e64e669d
Usman Iqbal <usmaniqbal225@hotmail.com>
2021-06-04 12:55:19 +0100

    updated password change work

--- password change backend.php
+++ password change backend.php
@@ -1,21 +1,48 @@
 <?php
-session_start();
-
+//include 'change password.php';
 $conn = new mysqli('localhost', 'root', '', 'accessaciti');
-$username = $_SESSION["username"];
-$oldpassword = $_POST['old password'];
-$newpassword = $_POST['new password'];
-$verifypassword = $_POST['verify password'];
-$result = $conn->query("SELECT password FROM users WHERE login='$username'");
-if (!$result) {
-        echo "Username entered does not exist";
-} else if ($password != $conn->query($result, 0)) {
-        echo "Incorrect password";
-}
-if ($newpassword = $verifypassword)
-        $sql = $conn->query("UPDATE users SET password='$newpassword' where login='$username'");
-if ($sql) {
-        echo "Password Changed";
-} else {
-        echo "The new password and confirm new password fields must be the same";
-}
+if (isset($_POST['Change password']));
+//else $error = "An error has occured, go to previous page";
+extract($_POST);
+
+if (isset($_SESSION["valid"]) && $_SESSION["valid"]) :
+$username = $_SESSION['username'];
+
+$password_hash = password_hash($newpassword, PASSWORD_BCRYPT);
+
+$oldpassword = $_POST['oldpassword'];
+$newpassword = $_POST['newpassword'];
+$verifypassword = $_POST['verifypassword'];
+if ($oldpassword != "" && $newpassword != "" && $verifypassword != "") :
+
+    if ($newpassword == $verifypassword) :
+        if ($newpassword != $oldpassword) :
+            $sql = "SELECT * FROM user WHERE user_id = " . $_SESSION["userid"] . ";";
+            $conn_check = $conn->query($sql);
+            if (password_verify($oldpassword, $conn_check->fetch_assoc()['password_hash'])) :
+                $fetch = $conn->query("UPDATE user SET 'password_hash` = '$newpassword' WHERE username ='$username'");
+                if ($fetch === True) 
+                    {
+                        header ("location: Password changed.html");
+                        exit();
+                    }else 
+                    {
+                        echo "Not successfull";
+                    }            
+                $oldpassword = '';
+                $newpassword = '';
+                $verifypassword = '';
+                $msg_sucess = "Password successfully updated!";
+            else :
+                echo "Old password is incorrect. Please try again.";
+            endif;
+        else :
+            echo "Old password and new password are the same. Please try again.";
+        endif;
+    else :
+        echo "New password and confirm password do not match.";
+    endif;
+else :
+    echo "Please fill all the fields";
+endif;   
+        endif;
