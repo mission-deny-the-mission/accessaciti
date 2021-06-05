e48eafc80ec3dbf46f8cd2fc736d8eda95150eee
mission-deny-the-mission <mission.deny.the.mission@gmail.com>
2021-06-05 14:47:46 +0100

    Fix missing else and indentation in password change backend.php

--- password change backend.php
+++ password change backend.php
@@ -6,43 +6,44 @@ if (isset($_POST['Change password']));
 extract($_POST);
 
 if (isset($_SESSION["valid"]) && $_SESSION["valid"]) :
-$username = $_SESSION['username'];
+    $username = $_SESSION['username'];
 
-$password_hash = password_hash($newpassword, PASSWORD_BCRYPT);
+    $password_hash = password_hash($newpassword, PASSWORD_BCRYPT);
 
-$oldpassword = $_POST['oldpassword'];
-$newpassword = $_POST['newpassword'];
-$verifypassword = $_POST['verifypassword'];
-if ($oldpassword != "" && $newpassword != "" && $verifypassword != "") :
-
-    if ($newpassword == $verifypassword) :
-        if ($newpassword != $oldpassword) :
-            $sql = "SELECT * FROM user WHERE user_id = " . $_SESSION["userid"] . ";";
-            $conn_check = $conn->query($sql);
-            if (password_verify($oldpassword, $conn_check->fetch_assoc()['password_hash'])) :
-                $fetch = $conn->query("UPDATE user SET 'password_hash` = '$newpassword' WHERE username ='$username'");
-                if ($fetch === True) 
+    $oldpassword = $_POST['oldpassword'];
+    $newpassword = $_POST['newpassword'];
+    $verifypassword = $_POST['verifypassword'];
+    if ($oldpassword != "" && $newpassword != "" && $verifypassword != "") :
+        if ($newpassword == $verifypassword) :
+            if ($newpassword != $oldpassword) :
+                $sql = "SELECT * FROM user WHERE user_id = " . $_SESSION["userid"] . ";";
+                $conn_check = $conn->query($sql);
+                if (password_verify($oldpassword, $conn_check->fetch_assoc()['password_hash'])) :
+                    $fetch = $conn->query("UPDATE user SET 'password_hash` = '$newpassword' WHERE username ='$username'");
+                    if ($fetch === True) 
                     {
                         header ("location: Password changed.html");
                         exit();
-                    }else 
+                    } else 
                     {
                         echo "Not successfull";
                     }            
-                $oldpassword = '';
-                $newpassword = '';
-                $verifypassword = '';
-                $msg_sucess = "Password successfully updated!";
+                    $oldpassword = '';
+                    $newpassword = '';
+                    $verifypassword = '';
+                    $msg_sucess = "Password successfully updated!";
+                else :
+                    echo "Old password is incorrect. Please try again.";
+                endif;
             else :
-                echo "Old password is incorrect. Please try again.";
+                echo "Old password and new password are the same. Please try again.";
             endif;
         else :
-            echo "Old password and new password are the same. Please try again.";
+            echo "New password and confirm password do not match.";
         endif;
     else :
-        echo "New password and confirm password do not match.";
+        echo "Please fill all the fields";
     endif;
-else :
-    echo "Please fill all the fields";
-endif;   
-        endif;
+else:
+    echo "You are not logged in";
+endif;
