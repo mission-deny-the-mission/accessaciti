d2dbc0b922bd29c33056329c5e7acde07827cbb0
A-Bolton <83464867+A-Bolton@users.noreply.github.com>
2021-06-06 13:22:41 +0100

    Handles the rating system.

---
+++ adjust_rating.php
@@ -0,0 +1,39 @@
+<?php
+
+$con = new mysqli('localhost', 'root', '', 'accessaciti');
+
+$direction = $_POST['hid_direction'];
+$adjuster = intval($direction);
+$issue = $_POST['hid_issID'];
+
+$sql1 = "SELECT `current_rating`, `rating_count` FROM `issue` WHERE `issue_id` = '$issue'";
+$rs1 = mysqli_query($con, $sql1);
+$row = mysqli_fetch_assoc($rs1);
+$current = $row['current_rating'];
+$count = $row['rating_count'];
+
+++$count;
+$current = $current + $adjuster;
+$percentage = ($current / $count) * 100;
+
+if ($percentage >= 75)
+{
+  $text = 'Most users found this report very helpful.';
+}
+elseif ($percentage >= 50)
+{
+  $text = 'Most users found this report helpful.';
+}
+elseif ($percentage >= 0)
+{
+  $text = 'Most users found this report unhelpful.';
+}
+else
+{
+  $text = 'Most users found this report very unhelpful.';
+}
+
+$sql2 = "INSERT INTO `issue` (`rating_text`, `current_rating`, `rating_count`) VALUES ('$text', '$current', '$count')";
+$rs2 = mysqli_query($con, $sql2);
+
+?>
