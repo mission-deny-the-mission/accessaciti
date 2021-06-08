2408c33c49150d85799472f346e022806d5eeebd
mission-deny-the-mission <mission.deny.the.mission@gmail.com>
2021-06-08 16:09:12 +0100

    Apply bootstrap and validation to change email front end

--- change email.php
+++ change email.php
@@ -9,14 +9,17 @@ if (!(isset($_SESSION["valid"]) && $_SESSION["valid"])) {
 
 <html>
     <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
         <title>Change email address</title>
     </head>
     <body>
         <h1>Change email:</h1>
         <form method="POST" action="change email handle.php">
-            <label for="old_email">New Email</label></br>
-            <input type="text" name="new_email"><br>
-            <input type="submit" name="Save">
+            <div class="form-group">
+                <label for="new_email">New Email</label></br>
+                <input type="email" name="new_email" class="form-control" required><br>
+            </div>
+            <input type="submit" name="Save" class="btn btn-primary">
         </form>
     </body>
 </html>
