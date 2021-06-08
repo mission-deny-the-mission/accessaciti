1ca4e6e77e60f7e798bbe1bf8560a3112b05b93a
mission-deny-the-mission <mission.deny.the.mission@gmail.com>
2021-06-08 16:15:03 +0100

    Apply validation and bootstrap to change password front end

--- change password.php
+++ change password.php
@@ -9,19 +9,26 @@ if (!(isset($_SESSION["valid"]) && $_SESSION["valid"])) {
 
 <html>
     <head>
-        <link rel="stylesheet" href="main.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
         <title>Change password</title>
     </head>
     <body>
         <h1>Change password:</h1>
         <form method="POST" action="password change backend.php">
-            <label for="old password">Old password</label><br>
-            <input type="password" name="oldpassword" id="old password"><br>
-            <label for="new password">New password</label><br>
-            <input type="password" name="newpassword" id="new password"><br>
-            <label for="repeat password">Verify password:</label><br>
-            <input type="password" name="verifypassword" id="verify password"><br>
-            <input type="submit" name="Change password">
+            <div class="form-group">
+                <label for="old password">Old password</label><br>
+                <input type="password" name="oldpassword" id="old password" class="form-control"
+                placeholder="Old Password" required><br>
+            </div>
+            <div class="form-group">
+                <label for="new password">New password</label><br>
+                <input type="password" name="newpassword" id="new password"
+                 class="form-control" placeholder="Password" required><br>
+                <label for="repeat password">Verify password:</label><br>
+                <input type="password" name="verifypassword" id="verify password"
+                class="form-control" placeholder="Verify Password" required><br>
+            </div>
+            <input type="submit" name="Change password" class="btn btn-primary">
         </form>
     </body>
 </html>
