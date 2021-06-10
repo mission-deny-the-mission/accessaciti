9e5b0dc6f9c9997671c16c6f87c7371add0a44dc
Harry Hall <mission.deny.the.mission@gmail.com>
2021-06-10 23:37:59 +0100

    fix delete user account.php

--- Delete user account.php
+++ Delete user account.php
@@ -8,25 +8,26 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if (isset($_SESSION["valid"]) && $_SESSION["valid"]) :
+if (isset($_SESSION["valid"]) && $_SESSION["valid"]){
 
-    $user_id = $_SESSION['username'];
+    $user_id = $_SESSION['user_id'];
     $newpassword = $_POST['password'];
     $password_hash = password_hash($newpassword, PASSWORD_BCRYPT);
-    $conn_check = $conn->query("SELECT password_hash FROM user = '$password_hash' WHERE username ='user_id'");
+    $conn_check = $conn->query("SELECT password_hash FROM user WHERE user_id = $user_id");
     
-    if (password_verify($newpassword, $conn_check['password_hash'])) :
-        $fetch = $conn->query("DELETE FROM users WHERE user_id='$user_id'");
-        if ($fetch === True) 
+    if (password_verify($newpassword, $conn_check['password_hash'])) {
+        $fetch = $conn->query("DELETE FROM FavoriteIssues WHERE user_id = $user_id");
+        $fetch2 = $conn->query("DELETE FROM users WHERE user_id = $user_id");
+        if ($fetch === True && $fetch2 === True) 
         {
             header ("location: DeleteAccSuccessful.html");
-           exit();
+            exit();
         } else 
         {
             echo "Error deleting record: " . $conn->error;
-            
-    }endif;
+        }
+    }
 
-else:
-echo "You are not logged in";
-endif;
+} else {
+    echo "You are not logged in";
+}
